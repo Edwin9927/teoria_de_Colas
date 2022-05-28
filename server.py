7fe7a154b503208aa4bc1920393b1b075eeb8589
Edwin Manzano <edwinmanzanoquinzo@gmail.com>
2022-05-28 01:53:36 -0500

    v1.1

--- server.py
+++ server.py
@@ -1,18 +1,28 @@
 from flask import Flask, request, render_template
 
+from static.codes.PICM import picm
+
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
-
-
-@app.route("/+", methods=['POST', 'GET'])
-def index_():
     if request.method == 'POST':
         data = request.form.to_dict()
-        print(data)
+        obj_picm = picm(int(data["c"]), float(data["mu"]), float(data["lambda"]))
+        if obj_picm.estabilidadSistema():
+            print("p0=", obj_picm.p_cero())
+            print("pk=", obj_picm.p_k())
+            print("p1=", obj_picm.p_n(2))
+            print("l=", obj_picm.l())
+            print("lq=", obj_picm.l_q())
+            print("ln=", obj_picm.l_n())
+            print("w=", obj_picm.w())
+            print("wq=", obj_picm.w_q())
+            print("wn=", obj_picm.w_n())
+            print(data)
+        else:
+            print("Sistema no valido")
         return "Datos recibidos"
     elif request.method == 'GET':
         return render_template('index.html')
---
+++ static/codes/PICM.py
@@ -0,0 +1,89 @@
+from math import factorial
+
+
+class picm:
+
+    def __init__(self, k, mu, _lambda):
+        self.k = k
+        self.mu = mu
+        self._lambda = _lambda
+
+    def rho_k(self):
+        return self._lambda / (self.k * self.mu)
+
+    def rho(self):
+        return self._lambda / self.mu
+
+    def estabilidadSistema(self) -> object:
+        if self.rho_k() > 1:
+            return False
+        else:
+            return True
+
+    def calculoPrevio(self):
+        return (1 / factorial(self.k)) * ((self.rho()) ** self.k) * (
+                (self.k * self.mu) / ((self.k * self.mu) - self._lambda))
+
+    # Calculo de p
+    # Probalidad de encontrar
+    def p_cero(self):
+        n = self.k - 1
+        sum1 = 0
+        while n >= 0:
+            sum1 += (1 / factorial(n)) * ((self.rho()) ** n)
+            n = n - 1
+        sum2 = self.calculoPrevio()
+        return 1 / (sum1 + sum2)
+
+    def p_k(self):
+        sum1 = self.calculoPrevio()
+        sum2 = self.p_cero()
+        return sum1 * sum2
+
+    def complemnto_p(self):
+        return 1 - self.p_k()
+
+    def p_n(self, n):
+        if self.k >= n:
+            part1 = 1 / (factorial(self.k) * (self.k ** (n - self.k)))
+            part2 = (self.rho()) ** n
+            part3 = self.p_cero()
+            return part3 * part2 * part1
+        else:
+            parte_1 = self.p_cero() / factorial(n)
+            parte_2 = self.rho() ** n
+            return parte_1 * parte_2
+
+    # Calculo de L
+    # Numero esperado de
+    def l_part1(self):
+        return (self.mu * self._lambda) * (self.rho() ** self.k)
+
+    def l_part2(self):
+        return (factorial(self.k - 1)) * (((self.k * self.mu) - self._lambda) ** 2)
+
+    def l(self):
+        return ((self.l_part1() / self.l_part2()) * self.p_cero()) + self.rho()
+
+    def l_q(self):
+        return (self.l_part1() / self.l_part2()) * self.p_cero()
+
+    def l_n(self):
+        return self.l_q() / self.p_k()
+
+    # Calculo de W
+    # tiempo esperado
+    def w_part1(self):
+        return self.mu * (self.rho() ** self.k)
+
+    def w_part2(self):
+        return self.l_part2()
+
+    def w(self):
+        return ((self.w_part1() / self.w_part2()) * self.p_cero()) + self.rho()
+
+    def w_q(self):
+        return (self.w_part1() / self.w_part2()) * self.p_cero()
+
+    def w_n(self):
+        return self.w_q() / self.p_k()
