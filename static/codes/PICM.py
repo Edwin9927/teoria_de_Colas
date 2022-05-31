6d2991e3a05015d659728d0a5481da8464ac7b89
JohnnyGaona <103288332+JohnnyGaona@users.noreply.github.com>
2022-05-31 10:11:41 -0500

    Creacion de funciones costo
    Están las funciones de costo total y unitario. pero las de unitario aun faltan definir

--- static/codes/PICM.py
+++ static/codes/PICM.py
@@ -3,10 +3,12 @@ from math import factorial
 
 class picm:
 
-    def __init__(self, k, mu, _lambda):
+    def __init__(self, k, mu, _lambda, h_labo, c_s):
         self.k = k
         self.mu = mu
         self._lambda = _lambda
+        self.h_labo = h_labo
+        self.c_s = c_s
 
     def rho_k(self):
         return self._lambda / (self.k * self.mu)
@@ -95,3 +97,36 @@ class picm:
 
     def w_n(self):
         return self.w_q() / self.p_k()
+
+    #Calculo de Costos
+    #Costos unitarios
+      
+    #C. unitaio en cola
+    def c_u_c(self):
+      return
+      
+    #C. unitario sistema
+    def c_u_ts(self):
+      return
+      
+    #C. unitario servicio
+    def c_u_tse(self):
+      return 
+      
+    #C. totales
+      
+    #C. diario tiempo cola
+    def c_t_te(self):
+      return self.lambda * self.h_labo * self.w_q() * self.c_u_c()
+      
+    #C. diario tiempo sistema
+    def c_t_ts(self):
+      return self.lambda * self.h_labo * self.w() * self.c_u_ts()
+      
+    #C. diario tiempo servicio
+    def c_t_tse(self):
+      return self.lambda * self.h_labo * (1/self.mu) *  self.c_u_tse()
+      
+    #C. diarios servidor
+    def c_t_s(self):
+      return self.k * self.c_s
