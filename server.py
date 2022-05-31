3c2d1012787845258fcfb9932143c87b1f0de83f
Edwin Manzano <edwinmanzanoquinzo@gmail.com>
2022-05-31 01:35:45 -0500

    v3

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 
+from static.codes.PFCM import pfcm
 from static.codes.PICM import picm
 
 app = Flask(__name__)
@@ -10,38 +11,58 @@ def index():
     if request.method == 'POST':
         data = request.form.to_dict()
         print(data)
-        obj_picm = picm(int(data["k"]), float(data["mu"]), float(data["lambda"]))
-        if obj_picm.estabilidadSistema():
-            print("p0=", obj_picm.p_cero())
-            print("pk=", obj_picm.p_k())
-            print("p3=", obj_picm.p_n(3))
-            print("l=", obj_picm.l())
-            print("lq=", obj_picm.l_q())
-            print("ln=", obj_picm.l_n())
-            print("w=", obj_picm.w())
-            print("wq=", obj_picm.w_q())
-            print("wn=", obj_picm.w_n())
-            print("pn", obj_picm.generarPn())
-            print(data)
-        else:
-            print("Sistema no valido")
-        return render_template(
-            'data.html',
-            nav=data,
-            rho=obj_picm.rho(),
-            rho_k=obj_picm.rho_k(),
-            estabilidad=obj_picm.estabilidadSistema(),
-            p0=obj_picm.p_cero(),
-            pk=obj_picm.p_k(),
-            pne=obj_picm.complemnto_p(),
-            l=obj_picm.l(),
-            lq=obj_picm.l_q(),
-            ln=obj_picm.l_n(),
-            w=obj_picm.w(),
-            wq=obj_picm.w_q(),
-            wn=obj_picm.w_n(),
-            pn=obj_picm.generarPn()
+        if data["m"] == '':
+            print("PI")
+            print(data["m"])
+            obj_picm = picm(int(data["k"]), float(data["mu"]), float(data["lambda"]))
+            if obj_picm.estabilidadSistema():
+                """print("p0=", obj_picm.p_cero())
+                print("pk=", obj_picm.p_k())
+                print("p0=", obj_picm.p_n(0))
+                print("l=", obj_picm.l())
+                print("lq=", obj_picm.l_q())
+                print("ln=", obj_picm.l_n())
+                print("w=", obj_picm.w())
+                print("wq=", obj_picm.w_q())
+                print("wn=", obj_picm.w_n())
+                print("pn", obj_picm.generarPn())
+                print(data)"""
+            else:
+                print("Sistema no valido")
+            return render_template(
+                'data.html',
+                nav=data,
+                rho=obj_picm.rho(),
+                rho_k=obj_picm.rho_k(),
+                estabilidad=obj_picm.estabilidadSistema(),
+                p0=obj_picm.p_cero(),
+                pk=obj_picm.p_k(),
+                pne=obj_picm.complemnto_p(),
+                l=obj_picm.l(),
+                lq=obj_picm.l_q(),
+                ln=obj_picm.l_n(),
+                w=obj_picm.w(),
+                wq=obj_picm.w_q(),
+                wn=obj_picm.w_n(),
+                pn=obj_picm.generarPn()
             )
+        else:
+            print("PF")
+            obj_pfcm = pfcm(int(data["k"]), float(data["mu"]), float(data["lambda"]), int(data["m"]))
+            print("p0_parte1", obj_pfcm.p_cero_parte1())
+            print("p0_parte2", obj_pfcm.p_cero_parte2())
+            print("p0", obj_pfcm.p_cero())
+            print(obj_pfcm.generarPn())
+            print("pe", obj_pfcm.p_e())
+            print("pne", obj_pfcm.p_ne())
+            print("l", obj_pfcm.l())
+            print("lq", obj_pfcm.l_q())
+            print("ln", obj_pfcm.l_n())
+            print("w", obj_pfcm.w())
+            print("wq", obj_pfcm.w_q())
+            print("wn", obj_pfcm.w_n())
+            return "Hola"
+
     return render_template('index.html')
 
 
---
+++ static/codes/PFCM.py
@@ -0,0 +1,107 @@
+from math import factorial
+
+
+class pfcm:
+
+    def __init__(self, k, mu, _lambda, m):
+        self.k = k
+        self.mu = mu
+        self._lambda = _lambda
+        self.m = m
+
+    def rho_k(self):
+        return self._lambda / (self.k * self.mu)
+
+    def rho(self):
+        return self._lambda / self.mu
+
+    def estabilidadSistema(self) -> object:
+        if self.rho_k() > 1:
+            return False
+        else:
+            return True
+
+    # Calculo de p
+    # Probalidad de encontrar
+    def m_fac(self) -> object:
+        return factorial(self.m)
+
+    def rho_n(self, n):
+        return self.rho() ** n
+
+    def p_cero_parte1(self):
+        sum1 = 0
+        for n in range(self.k):
+            sum1 += (self.m_fac() / (factorial(self.m - n) * factorial(n))) * self.rho_n(n)
+        return sum1
+
+    def p_cero_parte2(self):
+        sum2 = 0
+        for n in range(self.k, self.m + 1):
+            sum2 += (self.m_fac() / ((factorial(self.m - n)) * factorial(self.k) * (self.k ** (n - self.k)))) * self.rho_n(n)
+        return sum2
+
+    def p_cero(self):
+        return 1 / (self.p_cero_parte1() + self.p_cero_parte2())
+
+    def p_n(self, n):
+        if n >= self.k:
+            part1 = factorial(self.m) / (factorial(self.m - n) * (factorial(self.k) * (self.k ** (n - self.k))))
+            part2 = self.rho_n(n)
+            part3 = self.p_cero()
+            return part3 * part2 * part1
+        else:
+            parte_1 = (self.p_cero() * factorial(self.m)) / (factorial(self.m - n) * factorial(n))
+            parte_2 = self.rho() ** n
+            return parte_1 * parte_2
+
+    def generarPn(self):
+        cpn = self.p_cero()
+        dic = [{'n': '0', 'pn': str(cpn), 'cpn': str(cpn), 'ccpn': str(1-cpn)}]
+        for i in range(1, self.m + 1):
+            cpn += self.p_n(i)
+            dic.append({'n': i, 'pn': self.p_n(i), 'cpn': str(cpn), 'ccpn': str(1-cpn)})
+        return dic
+
+    def p_e(self):
+        pe = 0
+        for n in range(self.k):
+            pe += self.p_n(n)
+        return 1 - pe
+
+    def p_ne(self):
+        return 1 - self.p_e()
+
+    # Calculo de L
+    # Numero esperado de
+    def l_part1(self):
+        l1 = 0
+        for n in range(self.k):
+            l1 += n * self.p_n(n)
+        return l1
+
+    def l_part2(self):
+        l2 = 0
+        for n in range(self.k, self.m + 1):
+            l2 += (n - self.k) * self.p_n(n)
+        return l2
+
+    def l(self):
+        return self.l_part2() + self.l_part1() + (self.k * self.p_e())
+
+    def l_q(self):
+        return self.l_part2()
+
+    def l_n(self):
+        return self.l_q() / self.p_e()
+
+    # Calculo de W
+    # tiempo esperado
+    def w_q(self):
+        return self.l_q() / ((self.m - self.l()) * self._lambda)
+
+    def w(self):
+        return self.w_q() + (1 / self.mu)
+
+    def w_n(self):
+        return self.w_q() / self.p_e()
